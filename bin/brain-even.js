2fe6d99bf090abab8d3579f28839c6ae294c024a
ula9 <t.ulugbek.9@gmail.com>
2017-01-03 22:53:59 +0500

    regress

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,8 +1,3 @@
 #!/usr/bin/env node
 // @flow
-import readlineSync from 'readline-sync';
-
-console.log('Welcome to the Brain Games!\n');
-
-const name = readlineSync.question('What is your name? ');
-console.log(`Hello, ${name}!`);
+console.log('Welcome to the Brain Games!');
