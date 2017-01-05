e874d1245739174bac694ad0eb7694bb90ad2e30
ula9 <t.ulugbek.9@gmail.com>
2017-01-06 01:53:52 +0500

    add balance

---
+++ src/bin/brain-balance.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+/* @flow */
+
+import brainBalance from '../games/brain-balance';
+
+brainBalance();
---
+++ src/games/brain-balance.js
@@ -0,0 +1,25 @@
+// @flow
+
+import game from '../';
+
+const balance = (num) => {
+  const arr = String(num).split('').map(e => Number(e));
+  if (Math.abs(arr[0] - arr[arr.length - 1]) < 2) {
+    return Number(arr.sort().join(''));
+  }
+
+  arr[0] += 1;
+  arr[arr.length - 1] -= 1;
+
+  return balance(Number(arr.sort().join('')));
+};
+
+const description = 'balance the given number';
+
+const getQuestion = () => Math.round(Math.random() * 100000);
+
+const questionToString = (question: number) => `Question: ${question}`;
+
+const getCorrectAnswer = (question: number) => balance(question);
+
+export default () => game(description, getQuestion, getCorrectAnswer, questionToString);
